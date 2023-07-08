479035f4455f3b423e18ef990022d0fd21b92001
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-08 07:32:07 -0400

    add color comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Color } from "./components";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Couleur A feature rich color pallete.</h1>
+      <Color />
     </>
   );
 }
